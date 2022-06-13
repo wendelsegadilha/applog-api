553169b20014b8005d604a626043295869825ce2
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-06-13 19:14:55 -0300

    Projeto Spring Boot criado

---
+++ src/test/java/xyz/wendelsegadilha/applog/ApplogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.wendelsegadilha.applog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
