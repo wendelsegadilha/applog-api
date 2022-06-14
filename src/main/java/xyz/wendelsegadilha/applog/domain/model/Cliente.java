6fcaf57a14591625b46006fd071b360aa1e4e858
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-06-13 21:37:29 -0300

    Implementando Collection Resource

---
+++ src/main/java/xyz/wendelsegadilha/applog/domain/model/Cliente.java
@@ -0,0 +1,15 @@
+package xyz.wendelsegadilha.applog.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+		
+}
