860c0e5e4431f350a9cd10714650cd94d57ad2e0
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-06-14 14:09:41 -0300

    Usando Jakarta Persistence JPA

--- src/main/java/xyz/wendelsegadilha/applog/controller/ClienteController.java
+++ src/main/java/xyz/wendelsegadilha/applog/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package xyz.wendelsegadilha.applog.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +13,15 @@ import xyz.wendelsegadilha.applog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Bob");
-		cliente1.setEmail("bob99@gmail.com");
-		cliente1.setTelefone("98 98674-9852");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("mariasilva@gmail.com");
-		cliente2.setTelefone("98 98745-9233");
-				
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+	
 	}
 
 }
--- src/main/java/xyz/wendelsegadilha/applog/domain/model/Cliente.java
+++ src/main/java/xyz/wendelsegadilha/applog/domain/model/Cliente.java
@@ -1,15 +1,35 @@
 package xyz.wendelsegadilha.applog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
+	
+	@Column(name = "nome")
 	private String nome;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 		
 }
